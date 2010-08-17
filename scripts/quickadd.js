8ab37eff2906111bc85145273da2c27580a0ccf9
kylejaster <kyle@rayogram.com>
2010-08-17 17:09:16 -0400

    quick add features

---
+++ scripts/quickadd.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#block-civicrm-4 h3').toggle(function() {
+              $('#block-civicrm-4 .content').show();
+            }, function() {
+              $('#block-civicrm-4 .content').hide();
+            });
+        });
